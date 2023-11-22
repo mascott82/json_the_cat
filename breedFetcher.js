0eeb078972ec7d2e33ef0759cc300a89dd73653c
mascott82 <m.a.scott@live.com>
2023-11-22 11:01:46 -0600

    Implementing the unit test with Mocha and Chai.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,26 @@
 const request = require('request');
 
-let targetUrl = 'https://api.thecatapi.com/v1/breeds/search';
-if (process.argv.length === 3) {
-  targetUrl = targetUrl + "?q=" + process.argv[2];
-}
-
-request(targetUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Can not access the given url: ${targetUrl}.`, error);
-    process.exit(1);
+const fetchBreedDescription = function(breedName, callback) {
+  let targetUrl = 'https://api.thecatapi.com/v1/breeds/search';
+  if (breedName) {
+    targetUrl = targetUrl + "?q=" + breedName;
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error occured  when trying to access ${targetUrl}.`, response);
-  }
+  request(targetUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
   
-  if (body === "[]") {
-    console.error(`Breed not found. ${process.argv[2]}`);
-    process.exit(1);
-  }
+    if (response.statusCode !== 200) {
+      callback('Error occured  when trying to access url.', null);
+    }
+    
+    if (body === "[]") {
+      callback('Breed not found.', null);
+    }
+  
+    const data = JSON.parse(body);
+    callback(null, data[0].description);
+  });
+};
 
-  const data = JSON.parse(body);
-  console.log('--- Type of Body ---\n', typeof(data), '\n--- ---');
-  console.log(data);
-  console.log('--- first entry in the data array ---\n', data[0].description, '--- ---');
-});
+module.exports = { fetchBreedDescription };
